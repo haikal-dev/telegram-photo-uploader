9e5511ea6c510cfea238e49bb57313bb7a640628
Haikal Azizan <mhaikalazizan@gmail.com>
2024-06-11 23:43:58 +0800

    Initial commit

---
+++ telegram-jpg.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if(len(sys.argv) != 2):
+        print('Invalid params')
+        sys.exit(0)
+
+    photo_url = sys.argv[1]
+    files={'photo':open(photo_url, 'rb')}
+
+    token = "xxx"
+    receiver = "123"
+
+    resp = requests.post(f"https://api.telegram.org/bot{token}/sendPhoto?chat_id={receiver}",files=files)
